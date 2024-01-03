32d427f1626db1fe606cc6e70eb6a20bd99a3707
Jesco <JescoVogt@web.de>
2024-01-03 18:15:20 +0100

    added jump
    ahsaidwo

---
+++ Runtime/Core/States/LinkedStateEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JescoDev.SmoothBrainStates.Attributes;
+using JescoDev.SmoothBrainStates.SubStates;
+using UnityEngine;
+
+namespace JescoDev.SmoothBrainStates.States {
+    [Serializable, SmoothStateMenuPath("Default/Linked Entry"), SmoothStateFixedIdentifier("Linked Entry")]
+    public class LinkedStateEntry : State {
+
+        [field:SerializeField, InputPort] public SmoothPort InputPort { get; protected set; }
+        [SerializeField] private string _targetPath;
+        
+        public LinkedStateEntry() : base("Linked Entry") { }
+        
+        protected internal override bool CanBeActivated() => true;
+
+        protected internal override ExecutableState ResolveActivation(SmoothPort incomingPort = null) {
+            State target = StateMashineMachine.GetStateByPath(_targetPath);
+            if (target == null) {
+                Debug.LogError($"Could not find state at path {_targetPath}");
+                return null;
+            }
+            
+            return target.ResolveActivation();
+        }
+    }
+}
